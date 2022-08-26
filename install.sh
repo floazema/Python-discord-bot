4b128f21451e0bba8a2a4731bfa2c12ef52ea9da
AntoninLaudon <antonin.laudon@epitech.eu>
2022-08-26 17:04:35 +0200

    [ADD] motus / snake / new class for emojis / install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+pip install discord
+
+# this is for the buttons
+pip install -U git+https://github.com/Pycord-Development/pycord
+pip install -U git+https://github.com/Rapptz/discord.py
--- main.py
+++ main.py
@@ -1,60 +1,16 @@
-import secret
+from gc import callbacks
+import secret, sys, discord, random, asyncio, time
+from site import venv
+from turtle import update
 from discord.ext import commands
-import discord
-import random
+from discord.ui import Button, View
 import data as quizz_data
 import data as pendu_data
+from emojis import Emojis
 
 intents=discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="+", intents=intents)
-
-class Emojis:
-    def __init__(self):
-        self.orange_square = ":orange_square:"
-        self.green_square = ":green_square:"
-        self.black_large_square = ":black_large_square:"
-        self.blue_square = ":blue_square:"
-        self.letters = {
-            "A": ":regional_indicator_a:",
-            "B": ":regional_indicator_b:",
-            "C": ":regional_indicator_c:",
-            "D": ":regional_indicator_d:",
-            "E": ":regional_indicator_e:",
-            "F": ":regional_indicator_f:",
-            "G": ":regional_indicator_g:",
-            "H": ":regional_indicator_h:",
-            "I": ":regional_indicator_i:",
-            "J": ":regional_indicator_j:",
-            "K": ":regional_indicator_k:",
-            "L": ":regional_indicator_l:",
-            "M": ":regional_indicator_m:",
-            "N": ":regional_indicator_n:",
-            "O": ":regional_indicator_o:",
-            "P": ":regional_indicator_p:",
-            "Q": ":regional_indicator_q:",
-            "R": ":regional_indicator_r:",
-            "S": ":regional_indicator_s:",
-            "T": ":regional_indicator_t:",
-            "U": ":regional_indicator_u:",
-            "V": ":regional_indicator_v:",
-            "W": ":regional_indicator_w:",
-            "X": ":regional_indicator_x:",
-            "Y": ":regional_indicator_y:",
-            "Z": ":regional_indicator_z:",
-        }
-        self.numbers = {
-            "0": ":zero:",
-            "1": ":one:",
-            "2": ":two:",
-            "3": ":three:",
-            "4": ":four:",
-            "5": ":five:",
-            "6": ":six:",
-            "7": ":seven:",
-            "8": ":eight:",
-            "9": ":nine:",
-        }
 emoji = Emojis()
 
 @bot.event
@@ -101,6 +57,86 @@ async def wait_message_juste_prix(message: discord.Message):
 # ██║╚██╔╝██║██╔══╝  ██║╚██╔╝██║██║   ██║██   ██║██║
 # ██║ ╚═╝ ██║███████╗██║ ╚═╝ ██║╚██████╔╝╚█████╔╝██║
 # ╚═╝     ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝  ╚════╝ ╚═╝
+
+# ███╗   ███╗ ██████╗ ████████╗██╗   ██╗███████╗
+# ████╗ ████║██╔═══██╗╚══██╔══╝██║   ██║██╔════╝
+# ██╔████╔██║██║   ██║   ██║   ██║   ██║███████╗
+# ██║╚██╔╝██║██║   ██║   ██║   ██║   ██║╚════██║
+# ██║ ╚═╝ ██║╚██████╔╝   ██║   ╚██████╔╝███████║
+# ╚═╝     ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝
+
+motus_data = {"answer": "", "running": False, "history": [], "index": 0, "last_message": None}
+
+def check_word_in_motus(word: str, answer: str):
+    output = ""
+
+    for i, letter in enumerate(word):
+        if letter == answer[i]:
+            output += emoji.square.green
+        elif letter in answer:
+            output += emoji.square.orange
+        else:
+            output += emoji.square.black
+    return output
+
+def write_word_emojis(word: str):
+    word_emojis = ""
+    for letter in word:
+        if (letter in emoji.letters):
+            word_emojis += emoji.letters[letter]
+        else:
+            word_emojis += emoji.square.black
+    return word_emojis
+
+def display_history(history: list):
+    history_emojis = ""
+    for word in history:
+        if (word == ""):
+            history_emojis += emoji.square.blue * len(motus_data["answer"])
+            history_emojis += "\n"
+            history_emojis += emoji.square.black * len(motus_data["answer"])
+            history_emojis += "\n"
+        else:
+            history_emojis += write_word_emojis(word)
+            history_emojis += "\n"
+            history_emojis += check_word_in_motus(word, motus_data["answer"])
+            history_emojis += "\n"
+    return history_emojis
+
+@bot.command()
+async def motus(ctx: commands.Context):
+    global motus_data
+    motus_data["history"] = ["" for i in range(6)]
+    motus_data["answer"] = (pendu_data.pendu_data[random.randint(0, len(pendu_data.pendu_data) - 1)]).upper()
+    print("MOTUS: " + motus_data["answer"])
+    motus_data["running"] = True
+    motus_data["index"] = 0
+    sent = await ctx.send(display_history(motus_data["history"]))
+    motus_data["last_message"] = [sent.id, sent.channel.id]
+
+@bot.listen('on_message')
+async def wait_message_motus(message: discord.Message):
+    global motus_data
+    if (motus_data["running"] and message.author.id != 991271491809316865 and len(message.content) == len(motus_data["answer"]) and message.content.lower() in pendu_data.pendu_data):
+        user_message = message.content.upper()
+        motus_data["history"][motus_data["index"]] = user_message
+        motus_data["index"] += 1
+        channel = bot.get_channel(motus_data["last_message"][1])
+        msg = await channel.fetch_message(motus_data["last_message"][0])
+        sent = await message.channel.send(display_history(motus_data["history"]))
+        motus_data["last_message"] = [sent.id, sent.channel.id]
+        if (user_message == motus_data["answer"]):
+            motus_data["running"] = False
+            await message.channel.send("Bravo !")
+            return
+        if (motus_data["index"] == 6):
+            await message.channel.send("Le mot était: " + motus_data["answer"])
+            motus_data["running"] = False
+            motus_data["answer"] = ""
+            motus_data["history"] = ["" for i in range(6)]
+            motus_data["index"] = 0
+            return
+        await msg.delete()
 
 # ██████╗ ███████╗███╗   ██╗██████╗ ██╗   ██╗
 # ██╔══██╗██╔════╝████╗  ██║██╔══██╗██║   ██║
@@ -205,6 +241,142 @@ async def ratio(ctx: commands.Context):
         await last_msg.add_reaction("🇮")
         await last_msg.add_reaction("🇴")
 
+# ███████╗███╗   ██╗ █████╗ ██╗  ██╗███████╗
+# ██╔════╝████╗  ██║██╔══██╗██║ ██╔╝██╔════╝
+# ███████╗██╔██╗ ██║███████║█████╔╝ █████╗  
+# ╚════██║██║╚██╗██║██╔══██║██╔═██╗ ██╔══╝  
+# ███████║██║ ╚████║██║  ██║██║  ██╗███████╗
+# ╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+
+is_snake = -1
+
+class Snake:
+    def __init__(self, userid: int, messageid: int, channelid: int):
+        self.lastupdate = time.time()
+        self.userid = userid
+        self.messageid = messageid
+        self.channelid = channelid
+        self.screen = [[0 for i in range(9)] for j in range(9)]
+        self.pos = [random.randint(3, 6), random.randint(3, 6)]
+        self.dir = random.choice(["right", "left", "up", "down"])
+        self.tail = []
+        self.snake = {"body": emoji.square.green, "head": emoji.square.crossed_green, "background": emoji.square.black, "apple": emoji.square.red}
+
+    def draw(self):
+        line = ""
+        for i, y in enumerate(self.screen):
+            for j, x in enumerate(y):
+                if i == self.pos[0] and j == self.pos[1]:
+                    line += self.snake["head"]
+                elif [i, j] in self.tail:
+                    line += self.snake["body"]
+                else:
+                    line +=self.snake["background"]
+            line += "\n"
+        return line
+    
+    def update(self):
+        print(self.tail)
+        if self.dir == "right":
+            self.pos[1] += 1
+        elif self.dir == "left":
+            self.pos[1] -= 1
+        elif self.dir == "up":
+            self.pos[0] -= 1
+        elif self.dir == "down":
+            self.pos[0] += 1
+        if self.pos[0] < 0:
+            self.pos[0] = 8
+        elif self.pos[0] > 8:
+            self.pos[0] = 0
+        elif self.pos[1] < 0:
+            self.pos[1] = 8
+        elif self.pos[1] > 8:
+            self.pos[1] = 0
+        if self.pos in self.tail:
+            return False
+        self.tail.append(self.pos[:])
+        if self.pos == [10, 10]:
+            self.tail.append(self.pos[:])
+        if len(self.tail) > 5:
+            self.tail.pop(0)
+        return True
+
+
+@bot.command()
+async def snake(ctx: commands.Context):
+    print("starting a new snake game: ", ctx.author.name)
+    snake = Snake(ctx.author.id, ctx.message.id, ctx.channel.id)
+
+    async def quit(interaction):
+        await interaction.message.delete()
+        return True
+
+    async def go_up(interaction):
+        await interaction.response.defer()
+        print("going up")
+        if snake.dir != "down":
+            snake.dir = "up"
+    async def go_down(interaction):
+        await interaction.response.defer()
+        print("going down")
+        if snake.dir != "up":
+            snake.dir = "down"
+    async def go_left(interaction):
+        await interaction.response.defer()
+        print("going left")
+        if snake.dir != "right":
+            snake.dir = "left"
+    async def go_right(interaction):
+        await interaction.response.defer()
+        print("going right")
+        if snake.dir != "left":
+            snake.dir = "right"
+
+    async def draw_buttons():
+        view = View()
+
+        buttons = []
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.gray))
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.green))
+        buttons[1].callback = go_up
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.grey))
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.grey))
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.red))
+        buttons[4].callback = quit
+
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.green))
+        buttons[5].callback = go_left
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.grey))
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.green))
+        buttons[7].callback = go_right
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.grey))
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.grey))
+
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.grey))
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.green))
+        buttons[11].callback = go_down
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.grey))
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.grey))
+        buttons.append(Button(label=" ", style=discord.ButtonStyle.grey))
+
+        for button in buttons:
+            view.add_item(button)
+        return view
+
+
+
+    msg = await ctx.send(snake.draw())
+    while True:
+        if (time.time() - snake.lastupdate) < 2.0:
+            continue
+        snake.lastupdate = time.time()
+        if not snake.update():
+            break
+        await msg.edit(content=snake.draw(), view = await draw_buttons())
+
+    
+
 # ██████╗ ██╗   ██╗███╗   ██╗    ██████╗  ██████╗ ████████╗
 # ██╔══██╗██║   ██║████╗  ██║    ██╔══██╗██╔═══██╗╚══██╔══╝
 # ██████╔╝██║   ██║██╔██╗ ██║    ██████╔╝██║   ██║   ██║   
@@ -212,4 +384,10 @@ async def ratio(ctx: commands.Context):
 # ██║  ██║╚██████╔╝██║ ╚████║    ██████╔╝╚██████╔╝   ██║   
 # ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝    ╚═════╝  ╚═════╝    ╚═╝   
 
+@bot.command()
+@commands.has_role("BOT ADMIN")
+async def stop(ctx: commands.Context):
+    print(f"Stopping...\nStopped by {ctx.author.name} / {ctx.author.id}")
+    await ctx.send(f"Stopping...\nStopped by {ctx.author.mention}")
+    sys.exit(0)
 bot.run(secret.secret)
