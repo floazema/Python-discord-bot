09adf703f2ace25324565b2cb460ba04a00d9b8f
cquentin <>
2022-09-06 11:59:21 +0200

    Update formating

--- chess_prog/Pieces/king.py
+++ chess_prog/Pieces/king.py
@@ -5,7 +5,7 @@ class King(Piece):
     def __init__(self, isWhite: bool, x: int, y: int):
         super().__init__(isWhite, x, y)
         self.icon = ":regional_indicator_k:"
-    
+
     def move(self, x: int, y: int, all_piece: list):
         x -= self.pos.x
         y -= self.pos.y
@@ -14,7 +14,7 @@ class King(Piece):
         self.pos.x += x
         self.pos.y += y
         return True
-    
+
     def eat(self, x: int, y: int, all_piece: list):
         temp = None
         for i in all_piece:
